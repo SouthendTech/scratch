2182da3ee2763899547598f54e61ac008bac0f37
Southend Tech <38668719+SouthendTech@users.noreply.github.com>
2023-05-11 22:18:12 +0000

    Added two blocks - to repeat word and to multiply word + 1

--- your-scratch-extension/index.js
+++ your-scratch-extension/index.js
@@ -17,10 +17,10 @@ class Scratch3YourExtension {
             id: 'yourScratchExtension',
 
             // name that will be displayed in the Scratch UI
-            name: 'Demo',
+            name: 'Southend Tech',
 
             // colours to use for your extension blocks
-            color1: '#000099',
+            color1: '#000033',
             color2: '#660066',
 
             // icons to display
@@ -41,7 +41,7 @@ class Scratch3YourExtension {
                     blockType: BlockType.REPORTER,
 
                     // label to display on the block
-                    text: 'My first block [MY_NUMBER] and [MY_STRING]',
+                    text: 'Print our [MY_STRING] [MY_NUMBER] times',
 
                     // true if this block should end a stack
                     terminal: false,
@@ -56,7 +56,7 @@ class Scratch3YourExtension {
                     arguments: {
                         MY_NUMBER: {
                             // default value before the user sets something
-                            defaultValue: 123,
+                            defaultValue: 2,
 
                             // type/shape of the parameter - choose from:
                             //     ArgumentType.ANGLE - numeric value with an angle picker
@@ -81,7 +81,61 @@ class Scratch3YourExtension {
                             type: ArgumentType.STRING
                         }
                     }
+                },
+                {
+                    // name of the function where your block code lives
+                    opcode: 'mySecondBlock',
+
+                    // type of block - choose from:
+                    //   BlockType.REPORTER - returns a value, like "direction"
+                    //   BlockType.BOOLEAN - same as REPORTER but returns a true/false value
+                    //   BlockType.COMMAND - a normal command block, like "move {} steps"
+                    //   BlockType.HAT - starts a stack if its value changes from false to true ("edge triggered")
+                    blockType: BlockType.REPORTER,
+
+                    // label to display on the block
+                    text: 'Print our [MY_STRING] [MY_NUMBER] +1 times',
+
+                    // true if this block should end a stack
+                    terminal: false,
+
+                    // where this block should be available for code - choose from:
+                    //   TargetType.SPRITE - for code in sprites
+                    //   TargetType.STAGE  - for code on the stage / backdrop
+                    // remove one of these if this block doesn't apply to both
+                    filter: [ TargetType.SPRITE, TargetType.STAGE ],
+
+                    // arguments used in the block
+                    arguments: {
+                        MY_NUMBER: {
+                            // default value before the user sets something
+                            defaultValue: 2,
+
+                            // type/shape of the parameter - choose from:
+                            //     ArgumentType.ANGLE - numeric value with an angle picker
+                            //     ArgumentType.BOOLEAN - true/false value
+                            //     ArgumentType.COLOR - numeric value with a colour picker
+                            //     ArgumentType.NUMBER - numeric value
+                            //     ArgumentType.STRING - text value
+                            //     ArgumentType.NOTE - midi music value with a piano picker
+                            type: ArgumentType.NUMBER
+                        },
+                        MY_STRING: {
+                            // default value before the user sets something
+                            defaultValue: 'hi',
+
+                            // type/shape of the parameter - choose from:
+                            //     ArgumentType.ANGLE - numeric value with an angle picker
+                            //     ArgumentType.BOOLEAN - true/false value
+                            //     ArgumentType.COLOR - numeric value with a colour picker
+                            //     ArgumentType.NUMBER - numeric value
+                            //     ArgumentType.STRING - text value
+                            //     ArgumentType.NOTE - midi music value with a piano picker
+                            type: ArgumentType.STRING
+                        }
+                    }
                 }
+
             ]
         };
     }
@@ -93,7 +147,22 @@ class Scratch3YourExtension {
      */
     myFirstBlock ({ MY_NUMBER, MY_STRING }) {
         // example implementation to return a string
-        return MY_STRING + ' : doubled would be ' + (MY_NUMBER * 2);
+        //return MY_STRING + ' : doubled would be ' + (MY_NUMBER * 2);
+        return MY_STRING.repeat(MY_NUMBER)
+    }
+    mySecondBlock ({ MY_NUMBER, MY_STRING }) {
+        // example implementation to return a string
+        //return MY_STRING + ' : doubled would be ' + (MY_NUMBER * 2);
+        //return MY_STRING.repeat(MY_NUMBER + 1)
+        //return MY_STRING.repeat(((MY_NUMBER) + 1))
+
+        //let num = MY_NUMBER + 1
+        //return MY_STRING.repeat(num)
+
+        //return MY_STRING.repeat(MY_NUMBER++)
+        
+        return MY_STRING.repeat(++MY_NUMBER)
+
     }
 }
 
